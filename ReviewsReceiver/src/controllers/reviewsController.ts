b23df60a4f89eb4fa8448a23012c86df8606a064
Shaked <shakedabdu50@gmail.com>
2024-05-20 19:24:09 +0300

    push reviews script

--- ReviewsReceiver/src/controllers/reviewsController.ts
+++ ReviewsReceiver/src/controllers/reviewsController.ts
@@ -5,6 +5,9 @@ class ReviewsController extends BaseConstroller<IReview> {
   async post(req, res) {
     super.post(req, res);
   }
+  async postBatch(req, res) {
+    super.post(req, res);
+  }
 }
 
 export default new ReviewsController(reviewsModel);
--- ReviewsReceiver/src/routes/reviewsRoutes.ts
+++ ReviewsReceiver/src/routes/reviewsRoutes.ts
@@ -4,5 +4,6 @@ import ReviewsController from '../controllers/reviewsController';
 const reviewsRouter = Router();
 
 reviewsRouter.post('/',  ReviewsController.post.bind(ReviewsController));
+reviewsRouter.post('/batch',  ReviewsController.postBatch.bind(ReviewsController));
 
 export default reviewsRouter;
