4f5bc874a6112a38359ab60e3115596ce4b31acf
Shaked <shakedabdu50@gmail.com>
2024-06-20 15:07:27 +0300

    init backend project (typescript basic project)

---
+++ backend/src/app.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+
+const app = express();
+
+const initApp = () => {
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  return new Promise<Express>((resolve, reject) => {
+    mongoose
+      .connect(process.env.DB_URL)
+      .then(() => {
+       
+        resolve(app);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+export = initApp;
