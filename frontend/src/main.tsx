29cb11b41ded0ceef3e62c352c9af724fe3ded92
Yoav Richter <richter.yoav30@gmail.com>
2024-06-20 15:31:50 +0300

    add svg half

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import './main.css';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -15,12 +17,24 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#294174'
+    }
+  }
+});
+
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <div style={{width: '100vw', height: '100vh'}}>
+          <RouterProvider router={router} />
+      </div>
+      </ThemeProvider>
     </StrictMode>
   );
 }
--- frontend/src/routes/_auth.tsx
+++ frontend/src/routes/_auth.tsx
@@ -1,10 +1,17 @@
+import { Grid } from '@mui/material';
+import AuthBlobs from '../components/AuthBlobs.tsx';
 import { Outlet, createFileRoute } from '@tanstack/react-router';
 
 const AuthLayout: React.FC = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+      <Grid container>
+      <Grid xs={6}>
+        <Outlet />
+        </Grid>
+      <Grid xs={6}>
+          <AuthBlobs/>
+        </Grid>
+      </Grid>
   );
 };
 
