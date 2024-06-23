3d12ed0a9c450341b62c1dc878e64c1804da553b
Shaked <shakedabdu50@gmail.com>
2024-06-23 21:28:44 +0300

    add files from dev

---
+++ shared-types/schemas.ts
@@ -0,0 +1,33 @@
+import { Types as mongooseTypes } from 'mongoose';
+import { z } from 'zod';
+
+export const IClientSchema = z.object({
+  _id: z.instanceof(mongooseTypes.ObjectId).optional(),
+  email: z.string(),
+  fullName: z.string(),
+  businessName: z.string(),
+  businessDescription: z.string(),
+  businessId: z.string(),
+  password: z.string(),
+  tokens: z.array(z.string()).optional(),
+});
+
+export const ICreateUserSchema = IClientSchema.pick({
+  email: true,
+  fullName: true,
+  businessName: true,
+  businessDescription: true,
+  password: true,
+});
+
+export const ILoginFormDataSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+  remember: z.boolean().optional(),
+});
+
+export const IUserDetailsSchema = IClientSchema.omit({
+  _id: true,
+  password: true,
+  tokens: true,
+});
---
+++ shared-types/types.ts
@@ -0,0 +1,65 @@
+import { Types as mongooseTypes } from 'mongoose';
+import { IClientSchema, ICreateUserSchema, ILoginFormDataSchema, IUserDetailsSchema } from './schemas';
+import { z } from 'zod';
+
+export type IClient = z.infer<typeof IClientSchema>;
+
+export type IUserDetails = z.infer<typeof IUserDetailsSchema>;
+
+export type ICreateUser = z.infer<typeof ICreateUserSchema>;
+
+export type ILoginFormData = z.infer<typeof ILoginFormDataSchema>;
+
+export interface IUserTokens {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export enum DataSourceEnum {
+  API = 'api',
+  TRIPADVISOR = 'tripadvisor',
+  GOOGLE = 'google',
+}
+export interface IRawReview {
+  businessId: string;
+  value: string;
+  date: Date;
+  dataSource: DataSourceEnum;
+}
+
+export interface IReviewAnalaysis {
+  sentiment: SentimentEnum;
+  rating: number;
+  phrases: string[];
+}
+export type IReview = IRawReview & IReviewAnalaysis & { _id?: mongooseTypes.ObjectId };
+
+export interface IPieChartData {
+  id: number;
+  value: number;
+  label: string;
+}
+
+export interface ISentimentBarChartGroup {
+  date: string;
+  positive: number;
+  negative: number;
+  neutral: number;
+}
+
+export interface IWordFrequency {
+  text: string;
+  value: number;
+}
+
+export enum SentimentEnum {
+  POSITIVE = 'positive',
+  NEGATIVE = 'negative',
+  NEUTRAL = 'neutral',
+}
+
+export interface IBusinessAnalysis {
+  sentimentOverTime: ISentimentBarChartGroup[];
+  wordsFrequencies: IWordFrequency[];
+  dataSourceDistribution: IPieChartData[];
+}
