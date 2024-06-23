03c59f5d2991eda38d470c89d787ea1020e4506b
Yoav Richter <richter.yoav30@gmail.com>
2024-06-23 17:40:35 +0300

    fix route in navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -15,7 +15,7 @@ interface IMenuItem {
     link: string,
 }
 
-const navbarItems: Array<IMenuItem> = [{text: 'Home', icon: <HomeIcon/>, link: '/'},
+const navbarItems: Array<IMenuItem> = [{text: 'Home', icon: <HomeIcon/>, link: '/home'},
                                        {text: 'Reviews', icon: <MenuIcon/>, link: '/reviews'},
                                        {text: 'Settings', icon: <SettingsIcon/>, link: '/settings'}];
 
