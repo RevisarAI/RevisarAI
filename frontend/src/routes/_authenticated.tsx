8c66dba101f83a9cb6c8b42d69234e5e66b02032
Yoav Richter <richter.yoav30@gmail.com>
2024-06-23 17:53:10 +0300

    Dynamic page name at the top + uppercase

--- frontend/src/routes/_authenticated.tsx
+++ frontend/src/routes/_authenticated.tsx
@@ -1,8 +1,11 @@
-import { Outlet, createFileRoute, redirect } from '@tanstack/react-router';
+import { Outlet, createFileRoute, redirect, useRouterState, } from '@tanstack/react-router';
+
 import { Grid, Typography } from '@mui/material';
 import Navbar from '@/components/Navbar';
 
 const SiteLayout: React.FC = () => {
+  const router = useRouterState();
+
   return (
     <Grid container spacing={'2vw'}>
       <Grid container item md={3} height="100vh">
@@ -10,7 +13,9 @@ const SiteLayout: React.FC = () => {
         </Grid>
         <Grid container item md={9} sx={{ backgroundColor: 'primary.light'}} direction="column" justifyContent="flex-start">
           <Grid item sx={{marginTop: '3vh', marginBottom: '3vh'}}>
-            <Typography sx={{fontWeight: 'lighter'}}>Page name</Typography>
+            <Typography sx={{fontWeight: 'lighter'}}>
+              {router.location.pathname[1].toUpperCase() + router.location.pathname.slice(2)}
+              </Typography>
             </Grid>
           <Grid item><Outlet/></Grid>
         </Grid>
