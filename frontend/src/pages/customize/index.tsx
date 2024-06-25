e6313a334ef8d7aa84de4e824a683ab5feaad94a
Yair Chen <yair.chen2000@gmail.com>
2024-06-25 18:35:49 +0300

    fix(customize): refactor index

--- frontend/src/pages/customize/index.tsx
+++ frontend/src/pages/customize/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography, TextField, Box, Paper } from '@mui/material';
+import { Grid, Typography, TextField, Paper, Stack } from '@mui/material';
 import { useForm } from '@tanstack/react-form';
 import { IBusinessDetails } from 'shared-types';
 
@@ -15,57 +15,55 @@ const CustomizePage: React.FC = () => {
 
   return (
     <>
-      <Grid container justifyContent="center">
+      <Grid container justifyContent={'center'} spacing={4}>
         <Grid item md={12}>
           <Typography variant="h6" sx={{ fontWeight: 'regular' }}>
             Customize your recommendations and settings
           </Typography>
         </Grid>
+        <Grid item md={9}>
           <Paper
-          elevation={1}
+            elevation={1}
             sx={{
               backgroundColor: 'white',
               borderRadius: '1vh',
               height: '60vh',
             }}
           >
-            <Grid container sx={{ height: '60vh' }} direction="column" justifyContent='center'>
-              <Grid md={7} item>
-                <customizeForm.Field
-                  name="businessName"
-                  children={(field) => (
-                    <TextField
-                      fullWidth
-                      id="businessName-input"
-                      label="Business Name"
-                      type="string"
-                      variant="outlined"
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                  )}
-                ></customizeForm.Field>
-              </Grid>
-              <Grid md={7} item>
-                <customizeForm.Field
-                  name="businessDescription"
-                  children={(field) => (
-                    <TextField
-                      fullWidth
-                      id="businessDescription-input"
-                      label="Business Description"
-                      type="string"
-                      variant="outlined"
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={(e) => field.handleChange(e.target.value)}
-                    />
-                  )}
-                ></customizeForm.Field>
-              </Grid>
-            </Grid>
+            <Stack direction={'column'} padding={8} spacing={2}>
+              <customizeForm.Field
+                name="businessName"
+                children={(field) => (
+                  <TextField
+                    fullWidth
+                    id="businessName-input"
+                    label="Business Name"
+                    type="string"
+                    variant="outlined"
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                )}
+              ></customizeForm.Field>
+              <customizeForm.Field
+                name="businessDescription"
+                children={(field) => (
+                  <TextField
+                    fullWidth
+                    id="businessDescription-input"
+                    label="Business Description"
+                    type="string"
+                    variant="outlined"
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                )}
+              ></customizeForm.Field>
+            </Stack>
           </Paper>
+        </Grid>
       </Grid>
     </>
   );
