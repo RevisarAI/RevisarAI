1bd0006dd0801c33ac161d1942a01831eec63fce
Liad Yacobi <liadyacobi@gmail.com>
2024-06-26 15:32:31 +0300

    wip: loader

--- frontend/src/components/reviews/ReviewResponse.tsx
+++ frontend/src/components/reviews/ReviewResponse.tsx
@@ -1,11 +1,13 @@
 import { Autorenew as RefreshIcon, Send as SendIcon } from '@mui/icons-material';
-import { Dialog, Divider, Grid, IconButton, Stack, TextField, Tooltip, Typography, Zoom } from '@mui/material';
+import { Dialog, Grid, IconButton, Stack, TextField, Tooltip, Typography, Zoom } from '@mui/material';
 import { random } from 'lodash';
 import { useEffect, useState } from 'react';
 import { ContentCopy as ClipboardIcon, Check as CopiedIcon } from '@mui/icons-material';
+import { GridLoader } from 'react-spinners';
 import { TypeAnimation } from 'react-type-animation';
+import { useTheme } from '@mui/material/styles';
 
-const splitStringWithDelays = (longString: string, minDelay = 100, maxDelay = 300): Array<string | number> => {
+const splitStringWithDelays = (longString: string, minDelay = 50, maxDelay = 300): Array<string | number> => {
   const getRandomDelay = () => random(minDelay, maxDelay);
   const getRandomLength = (minLength: number, maxLength: number) => random(minLength, maxLength);
 
@@ -13,7 +15,7 @@ const splitStringWithDelays = (longString: string, minDelay = 100, maxDelay = 30
   let currentIndex = 0;
 
   while (currentIndex < longString.length) {
-    const randomLength = getRandomLength(10, 40);
+    const randomLength = getRandomLength(30, 50);
     const substring = longString.slice(0, currentIndex + randomLength);
 
     result.push(substring, getRandomDelay());
@@ -30,8 +32,7 @@ interface ReviewResponseProps {
 }
 
 // TODO: replace lorem ipsum
-const lorem =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nibh nunc, laoreet vel ex non, ultrices ullamcorper nibh. Donec cursus neque egestas dui congue, ac dapibus orci pellentesque. Nulla a porta tellus. Ut scelerisque blandit dolor. Nulla sit amet mauris suscipit, mattis massa id, cursus tellus. Nam pulvinar interdum euismod. Maecenas sed eleifend nunc, sed congue arcu. Sed egestas nulla et purus sollicitudin hendrerit.';
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'.repeat(10);
 
 const ReviewResponse: React.FC<ReviewResponseProps> = ({ reviewText, open, onClose }) => {
   const [responseText, setResponseText] = useState<string>('');
@@ -40,6 +41,8 @@ const ReviewResponse: React.FC<ReviewResponseProps> = ({ reviewText, open, onClo
   const [prompt, setPrompt] = useState<string>('');
   const [copied, setCopied] = useState<boolean>(false);
   const [typeAnimationKey, setTypeAnimationKey] = useState<number>(0);
+
+  const theme = useTheme();
 
   // TODO: replace with actual query
   setTimeout(() => {
@@ -51,8 +54,6 @@ const ReviewResponse: React.FC<ReviewResponseProps> = ({ reviewText, open, onClo
   useEffect(() => {
     setTypeAnimationKey((prev) => prev + 1);
   }, [loading, finishedWritingResponse]);
-
-  const loadingSequence = ['Loading', 100, 'Loading.', 100, 'Loading..', 100, 'Loading...'];
 
   const copyResponse = () => {
     if (navigator.clipboard) {
@@ -71,69 +72,84 @@ const ReviewResponse: React.FC<ReviewResponseProps> = ({ reviewText, open, onClo
 
   return (
     <Dialog PaperProps={{ sx: { borderRadius: '1rem' } }} fullWidth onClose={onClose} open={open}>
-      <Stack direction="column" padding={5} spacing={3}>
+      <Stack
+        minHeight="30vh"
+        display="flex"
+        direction="column"
+        padding={5}
+        paddingBottom={4}
+        spacing={3}
+        justifyContent="space-between"
+      >
         <Stack direction="row" justifyContent="space-between" alignItems="center">
           <Typography variant="h5">{loading ? 'Generating a response...' : 'Generated response'}</Typography>
-          <Tooltip
-            arrow
-            TransitionComponent={Zoom}
-            TransitionProps={{ timeout: 100 }}
-            title={<Typography variant="body2">Copy response</Typography>}
-            placement="top"
-          >
-            <IconButton disabled={copied} onClick={copyResponse}>
-              {copied ? <CopiedIcon /> : <ClipboardIcon />}
-            </IconButton>
-          </Tooltip>
+          {finishedWritingResponse && (
+            <Tooltip
+              arrow
+              TransitionComponent={Zoom}
+              TransitionProps={{ timeout: 100 }}
+              title={<Typography variant="body2">Copy response</Typography>}
+              placement="top"
+            >
+              <IconButton disabled={copied} onClick={copyResponse}>
+                {copied ? <CopiedIcon /> : <ClipboardIcon />}
+              </IconButton>
+            </Tooltip>
+          )}
         </Stack>
-        <Stack direction="column" spacing={2} maxHeight="50vh" minHeight="30vh">
-          <Typography maxHeight="40vh" variant="body1" style={{ overflow: 'auto', whiteSpace: 'pre-line' }}>
-            {loading ? (
-              <TypeAnimation key={typeAnimationKey} sequence={loadingSequence} repeat={0} speed={75} />
-            ) : !finishedWritingResponse ? (
+        {loading || true ? (
+          <div style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <GridLoader color={theme.palette.primary.main} />
+          </div>
+        ) : (
+          <Typography
+            maxHeight="40vh"
+            minHeight="20vh"
+            variant="body1"
+            style={{ overflow: 'auto', whiteSpace: 'pre-line' }}
+          >
+            )
+            {!finishedWritingResponse ? (
               <TypeAnimation
                 key={typeAnimationKey}
                 style={{ whiteSpace: 'pre - line' }}
-                sequence={[...splitStringWithDelays(''), () => setFinishedWritingResponse(true)]}
+                sequence={[...splitStringWithDelays(responseText), () => setFinishedWritingResponse(true)]}
                 repeat={0}
-                speed={75}
+                speed={80}
               />
             ) : (
-              responseText.repeat(4)
+              responseText
             )}
           </Typography>
-          {!loading && finishedWritingResponse && (
-            <>
-              <Divider></Divider>
-              <Grid container direction="row" height="5vh" alignItems="center" justifyContent="center">
-                <Grid item md>
-                  <TextField
-                    variant="standard"
-                    fullWidth
-                    placeholder={'Enter a prompt or regenerate response automatically'}
-                    InputProps={{
-                      endAdornment: (
-                        <Tooltip
-                          arrow
-                          TransitionComponent={Zoom}
-                          TransitionProps={{ timeout: 100 }}
-                          title={
-                            <Typography variant="body2">Regenerate{prompt.length > 0 && ' with prompt'}</Typography>
-                          }
-                          placement="top"
-                        >
-                          <IconButton type="button">{prompt.length === 0 ? <RefreshIcon /> : <SendIcon />}</IconButton>
-                        </Tooltip>
-                      ),
-                    }}
-                    value={prompt}
-                    onChange={(e) => setPrompt(e.target.value)}
-                  />
-                </Grid>
+        )}
+        {!loading && finishedWritingResponse && (
+          <>
+            <Grid container direction="row" height="5vh" alignItems="center" justifyContent="center" paddingBottom={2}>
+              <Grid item md>
+                <TextField
+                  variant="standard"
+                  fullWidth
+                  placeholder={'Enter a prompt or regenerate response automatically'}
+                  InputProps={{
+                    endAdornment: (
+                      <Tooltip
+                        arrow
+                        TransitionComponent={Zoom}
+                        TransitionProps={{ timeout: 100 }}
+                        title={<Typography variant="body2">Regenerate{prompt.length > 0 && ' with prompt'}</Typography>}
+                        placement="top"
+                      >
+                        <IconButton type="button">{prompt.length === 0 ? <RefreshIcon /> : <SendIcon />}</IconButton>
+                      </Tooltip>
+                    ),
+                  }}
+                  value={prompt}
+                  onChange={(e) => setPrompt(e.target.value)}
+                />
               </Grid>
-            </>
-          )}
-        </Stack>
+            </Grid>
+          </>
+        )}
       </Stack>
     </Dialog>
   );
