96c1fa7b26a550669f3eb428eae55260d367684c
Liad Yacobi <liadyacobi@gmail.com>
2024-07-01 12:29:00 +0300

    send on enter

--- frontend/src/components/reviews/ReviewReply.tsx
+++ frontend/src/components/reviews/ReviewReply.tsx
@@ -168,6 +168,11 @@ const ReviewReply: React.FC<ReviewReplyProps> = ({ reviewText, open, onClose }) 
                   variant="standard"
                   fullWidth
                   inputProps={{ maxLength: 65 }}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      query.refetch();
+                    }
+                  }}
                   placeholder={'Prompt or regenerate new reply automatically'}
                   InputProps={{
                     // Input icon
