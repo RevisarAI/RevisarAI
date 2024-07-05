f2ca9656e77ff43f7d17a8ba3ce4ba24ac24825e
Yoav Richter <richter.yoav30@gmail.com>
2024-07-05 12:15:18 +0300

    add striktrough and custom render components

--- frontend/src/components/panels/actionitems/WeeklyActionItemsPanel.tsx
+++ frontend/src/components/panels/actionitems/WeeklyActionItemsPanel.tsx
@@ -1,46 +1,40 @@
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography } from '@mui/material';
 import ActionItemsTable, { ActionItemsColumn } from './ActionItemsTable';
-import { IActionItem } from "shared-types";
-import ActionItemText from "./ActionItemText";
-import ActionItemCheckbox from "./ActionItemCheckbox";
+import { IActionItem } from 'shared-types';
+import ActionItemCheckbox from './ActionItemCheckbox';
+import './WeeklyActionItemsPanel.css';
 
 interface ActionItemsPanelProps {
   data: IActionItem[];
+  height: number;
 }
 
 const columns: readonly ActionItemsColumn[] = [
-    {
-        id: 'value',
-        label: 'Title',
-        align: 'left',
-        minWidth: 71,
-        render: (item: IActionItem) => (
-            <ActionItemText key={item._id?.toString()} text={item.value} />
-        ),
-    },
-    {
-        id: 'isCompleted',
-        label: 'Is done',
-        align: 'left',
-        minWidth: 5,
-        render: (isCompleted: IActionItem['isCompleted']) => (
-            <ActionItemCheckbox isCompleted={isCompleted}/>
-        ),
-    }
+  {
+    id: 'value',
+    label: 'Title',
+    align: 'left',
+    minWidth: 71,
+    render: (value: IActionItem['value'], item: IActionItem) => <span className={item.isCompleted ? 'actionItemCompleted' : ''}>{value}</span>,
+  },
+  {
+    id: 'isCompleted',
+    label: 'Is done',
+    align: 'left',
+    minWidth: 5,
+    render: (isCompleted: IActionItem['isCompleted']) => <ActionItemCheckbox isCompleted={isCompleted} />,
+  },
 ];
 
-const WeeklyActionItemsPanel: React.FC<ActionItemsPanelProps> = ({ data }) => {
-    return (
-        <Paper style={{ padding: 16, borderRadius: 15 }}>
-        <Typography variant="body1" sx={{ fontWeight: 'semibold' }}>
-            Weekly action items
-        </Typography>
-        <ActionItemsTable
-        rows={data}
-        columns={columns}
-        />
-      </Paper>
-    );
+const WeeklyActionItemsPanel: React.FC<ActionItemsPanelProps> = ({ data, height }) => {
+  return (
+    <Paper style={{ padding: 16, borderRadius: 15, height: height }}>
+      <Typography variant="body1" sx={{ fontWeight: 'semibold' }}>
+        Weekly action items
+      </Typography>
+      <ActionItemsTable rows={data} columns={columns}/>
+    </Paper>
+  );
 };
 
 export default WeeklyActionItemsPanel;
