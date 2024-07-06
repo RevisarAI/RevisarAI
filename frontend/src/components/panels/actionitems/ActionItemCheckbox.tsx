2637345cfd7fa1ee91db05a09a50e00d91afa6be
Yoav Richter <richter.yoav30@gmail.com>
2024-07-06 12:52:46 +0300

    add function for sending request for updating the status of the action item

--- frontend/src/components/panels/actionitems/ActionItemCheckbox.tsx
+++ frontend/src/components/panels/actionitems/ActionItemCheckbox.tsx
@@ -4,10 +4,11 @@ import Checkbox from '@mui/material/Checkbox';
 
 interface ActionItemTextProps {
   isCompleted: boolean;
+  updateActionItemStatus: () => void;
 }
 
-const ActionItemCheckbox: React.FC<ActionItemTextProps> = ({ isCompleted }) => (
-  <Checkbox checked={isCompleted}/>
+const ActionItemCheckbox: React.FC<ActionItemTextProps> = ({ isCompleted, updateActionItemStatus }) => (
+  <Checkbox checked={isCompleted} onClick={updateActionItemStatus}/>
 );
 
 export default ActionItemCheckbox;
--- frontend/src/components/panels/actionitems/WeeklyActionItemsPanel.tsx
+++ frontend/src/components/panels/actionitems/WeeklyActionItemsPanel.tsx
@@ -3,36 +3,53 @@ import ActionItemsTable, { ActionItemsColumn } from './ActionItemsTable';
 import { IActionItem } from 'shared-types';
 import ActionItemCheckbox from './ActionItemCheckbox';
 import './WeeklyActionItemsPanel.css';
+import { actionItemsService } from '@/services/action-items-service';
+import { ObjectId } from 'mongoose';
 
 interface ActionItemsPanelProps {
   data: IActionItem[];
   height: number;
+  itemsID: string;
 }
 
-const columns: readonly ActionItemsColumn[] = [
-  {
-    id: 'value',
-    label: 'Title',
-    align: 'left',
-    minWidth: 50,
-    render: (value: IActionItem['value'], item: IActionItem) => <span className={item.isCompleted ? 'actionItemCompleted' : ''}>{value}</span>,
-  },
-  {
-    id: 'isCompleted',
-    label: 'Is done',
-    align: 'left',
-    minWidth: 5,
-    render: (isCompleted: IActionItem['isCompleted']) => <ActionItemCheckbox isCompleted={isCompleted} />,
-  },
-];
+const WeeklyActionItemsPanel: React.FC<ActionItemsPanelProps> = ({ data, height, itemsID }) => {
+  const updateActionItemStatus = (item: IActionItem, itemsID: string) => {
+    item.isCompleted = !item.isCompleted;
+    actionItemsService.updateActionItemStatus(item, itemsID);
+  };
 
-const WeeklyActionItemsPanel: React.FC<ActionItemsPanelProps> = ({ data, height }) => {
+  const columns: readonly ActionItemsColumn[] = [
+    {
+      id: 'value',
+      label: 'Title',
+      align: 'left',
+      minWidth: 50,
+      render: (value: IActionItem['value'], item: IActionItem) => (
+        <span className={item.isCompleted ? 'actionItemCompleted' : ''}>{value}</span>
+      ),
+    },
+    {
+      id: 'isCompleted',
+      label: 'Is done',
+      align: 'left',
+      minWidth: 5,
+      render: (isCompleted: IActionItem['isCompleted'], item: IActionItem) => (
+        <ActionItemCheckbox
+          isCompleted={isCompleted}
+          updateActionItemStatus={() => {
+            updateActionItemStatus(item, itemsID);
+          }}
+        />
+      ),
+    },
+  ];
+
   return (
     <Paper style={{ padding: 16, borderRadius: 15, height: height }}>
       <Typography variant="body1" sx={{ fontWeight: 'semibold' }}>
         Weekly action items
       </Typography>
-      <ActionItemsTable rows={data} columns={columns}/>
+      <ActionItemsTable rows={data} columns={columns} />
     </Paper>
   );
 };
