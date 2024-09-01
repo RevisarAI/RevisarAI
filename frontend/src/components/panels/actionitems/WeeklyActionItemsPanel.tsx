05eb86250b8f48b3022f1fb912fe6403e12cf20a
Yair Chen <yair.chen2000@gmail.com>
2024-09-01 19:58:16 +0300

    remove unused import

--- frontend/src/components/panels/actionitems/WeeklyActionItemsPanel.tsx
+++ frontend/src/components/panels/actionitems/WeeklyActionItemsPanel.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Grid, Paper, Typography } from '@mui/material';
+import { Checkbox, Paper, Typography } from '@mui/material';
 import ActionItemsTable, { ActionItemsColumn } from './ActionItemsTable';
 import { IActionItem } from 'shared-types';
 import './WeeklyActionItemsPanel.css';
